af5d3cb9126c22a23b3f347db3f7167cb89e6fa1
Ren <s0985514623@gmail.com>
2024-04-18 17:40:58 +0800

    chore: release v1.0.4

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       R2-Simple-Slider-With-ACF
  * Plugin URI:        https://github.com/s0985514623/R2-Simple-Slider-With-ACF
  * Description:       針對 ACF Repeater建立的簡單輪播器 短碼[simple-slider-with-acf]
- * Version:           1.0.3
+ * Version:           1.0.4
  * Requires PHP:      7.4
  * Requires Plugins:  advanced-custom-fields-pro
  * Author:            R2
