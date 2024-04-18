9c41f315ff6a5af7af8c753efceb69ff2b8c32f9
Ren <s0985514623@gmail.com>
2024-04-18 10:20:53 +0800

    fix:release-it

--- release/.release-it.cjs
+++ release/.release-it.cjs
@@ -11,49 +11,33 @@
  * @see https://github.com/release-it/release-it/blob/main/docs/configuration.md
  */
 
-const releasedPluginName = 'r2-wc-product-addon-release'
+const releasedPluginName = "r2-wc-product-addon-release";
 
 module.exports = {
   releasedPluginName,
   git: {
     commit: true,
-    commitMessage: 'chore: release v${version}',
+    commitMessage: "chore: release v${version}",
     tag: true,
-    tagName: 'v${version}',
-    commitArgs: ['-n'],
+    tagName: "v${version}",
+    commitArgs: ["-n"],
     push: true,
   },
   hooks: {
     // 'before:init': [], // run before initialization
     // 'after:[my-plugin]:bump': './bin/my-script.sh', // run after bumping version of my-plugin
-    'after:bump': [
-      'yarn build && echo ✅ build success && yarn sync:version && echo ✅ sync version success',
-      'yarn create:release && echo ✅ create release files success',
-      `cd release/${releasedPluginName}/${releasedPluginName} && composer install --no-dev && cd ../.. && echo ✅ composer install success`,
-      'yarn zip && echo ✅ create zip success',
-    ], // run after bumping version
+    "after:bump": ["yarn build && echo ✅ build success && yarn sync:version && echo ✅ sync version success", "yarn create:release && echo ✅ create release files success", `cd release/${releasedPluginName}/${releasedPluginName} && composer install --no-dev && cd ../.. && echo ✅ composer install success`, "yarn zip && echo ✅ create zip success"], // run after bumping version
     // 'after:git:release': 'echo After git push, before github release', // run after git push, before github release
-    'after:release': [
-      'git pull',
-    ], // run after release
+    "after:release": ["git pull"], // run after release
   },
   npm: {
     publish: false,
   },
   github: {
     release: true,
-    releaseName: 'v${version}',
+    releaseName: "v${version}",
     assets: [`./release/${releasedPluginName}.zip`], // relative path
     web: false,
   },
-  allowedItems: [
-    'inc',
-    'js/dist',
-    'required_plugins',
-    'composer.json',
-    'composer.lock',
-    'index.php',
-    'plugin.php',
-    'README.md',
-  ],
-}
+  allowedItems: ["inc", "css", "html", "js", "composer.json", "composer.lock", "plugin.php", "README.md"],
+};
